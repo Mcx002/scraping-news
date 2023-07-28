2079359c42c1d1226f68d19c8da168a7062a26fa
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-07-28 19:31:23 +0700

    Add retry and time out request

--- src/repositories/detik.py
+++ src/repositories/detik.py
@@ -1,4 +1,5 @@
 import requests
+from requests import Response
 
 
 def find_hello():
@@ -6,16 +7,22 @@ def find_hello():
 
 
 def find_articles(keyword, page):
-    page_query = ''
-    if page != 1:
-        page_query = '&page=' + str(page)
+    try:
+        page_query = ''
+        if page != 1:
+            page_query = '&page=' + str(page)
 
-    keyword = keyword.replace(' ', '+')
-    url = 'https://www.detik.com/search/searchall?query=' + keyword + page_query
+        keyword = keyword.replace(' ', '+')
+        url = 'https://www.detik.com/search/searchall?query=' + keyword + page_query
 
-    response = requests.get(url)
-    return response
+        response = requests.get(url, timeout=(3, 30))
+        return response
+    except requests.RequestException:
+        return None
 
 
 def find_document(url):
-    return requests.get(url)
+    try:
+        return requests.get(url, timeout=(3, 30))
+    except requests.RequestException:
+        return None
