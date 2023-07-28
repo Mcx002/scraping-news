f90ecb3405d299387b5d2c56c13cbf8e28029693
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-07-28 19:33:00 +0700

    feat(document): Add orange3 formatted data

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from src.services.detik import scrap as detik_scrap
 
 def prompt_menu():
     print('Menu:')
+    print('0. Stop Application')
     print('1. Scrap Detik')
 
     return input('Choose which media you wanna scrap for: ')
 
 
 def main():
-    menu = prompt_menu()
+    menu = '-1'
+    while menu != '0':
+        menu = prompt_menu()
 
-    match menu:
-        case "1":
-            detik_scrap()
-        case _:
-            print('Please input the menu')
-
+        match menu:
+            case "0":
+                print('Application stopped')
+            case "1":
+                detik_scrap()
+            case _:
+                print('Please input the correct menu')
 
 main()
